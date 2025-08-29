5f3cb998602532ba4bba163ab06ea03448517fe9
Rykon <81365827+CaptainRykon@users.noreply.github.com>
2025-08-30 05:06:40 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,21 +1,23 @@
 // src/lib/firebase.ts
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database"; // ✅ For Realtime DB
+import { getAnalytics } from "firebase/analytics";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrn0OZiqKwQZy5rgSf4533zOBFq6Zg6YQ",
-  authDomain: "flow-ea22e.firebaseapp.com",
-  databaseURL: "https://flow-ea22e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "flow-ea22e",
-  storageBucket: "flow-ea22e.firebasestorage.app",
-  messagingSenderId: "402723284558",
-  appId: "1:402723284558:web:fbc54eada0713dfaf8c51f",
-  measurementId: "G-QT78EN2X8B"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase once
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
+const analytics = getAnalytics(app);
 
 export { db };
