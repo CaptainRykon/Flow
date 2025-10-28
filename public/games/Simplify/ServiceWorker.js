208613f7c1edd0fd476b568b2522d3884b16b07f
Rykon <81365827+CaptainRykon@users.noreply.github.com>
2025-10-28 22:09:56 +0530

    x

---
+++ public/games/Simplify/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Rykon Studios-Classify Game-1";
+const contentToCache = [
+    "Build/Testing Games.loader.js",
+    "Build/Testing Games.framework.js",
+    "Build/Testing Games.data",
+    "Build/Testing Games.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
